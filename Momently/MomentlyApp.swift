624507413f81ff86a0bf3c086b24ff6db8e39dc8
Yohai Reshef <yohai.reshef@gmail.com>
2025-05-07 16:48:44 +0300

    Initial Commit

---
+++ Momently/MomentlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentlyApp.swift
+//  Momently
+//
+//  Created by Yohai on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MomentlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
